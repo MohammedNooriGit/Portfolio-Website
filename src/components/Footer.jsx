a7a8bdd9696d9461e5f051d9cda6747bebed2643
Bu Jasem <91452072+MohammedNooriGit@users.noreply.github.com>
2022-11-02 10:58:42 -0400

    Add current year

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
 export default function Footer() {
+  const currentYear = new Date().getFullYear;
   return (
     <div className="relative bg-first w-full">
       <div className="text-center text-swhite p-5">
-        <p>© Mohammed Noori</p>
+        <p>© {currentYear} Mohammed Noori</p>
       </div>
     </div>
   );
