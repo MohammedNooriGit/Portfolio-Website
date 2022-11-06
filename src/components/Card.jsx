dd14fea0a0046c8f427b407d57e18eda8b9ddb3c
Bu Jasem <91452072+MohammedNooriGit@users.noreply.github.com>
2022-11-06 08:30:57 -0500

    Remove unused import

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 import data from "../data";
 import { FaGithub } from "react-icons/fa";
 
